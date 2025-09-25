58df3fc44877b18f50da9e69bd7cd710e31a0732
Grikhanov Artem <artemgrihanov@ya.ru>
2025-09-25 19:21:05 +0300

    sm changes

--- lr1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/agrikhanov/Desktop/oop_2025_labs/lr1
-# Build directory: /Users/agrikhanov/Desktop/oop_2025_labs/lr1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/agrikhanov/Desktop/oop_2025_labs/lr1/build/run_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- lr1/build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/agrikhanov/Desktop/oop_2025_labs/lr1/build/_deps/googletest-src/googlemock
-# Build directory: /Users/agrikhanov/Desktop/oop_2025_labs/lr1/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
